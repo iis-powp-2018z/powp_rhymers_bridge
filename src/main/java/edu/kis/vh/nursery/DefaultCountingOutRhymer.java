e37c8446c44a1959e57a1d2be24689f1db63658f
monika <156430@edu.p.lodz.pl>
2018-10-07 17:42:26 +0200

    Laboratorium - Punkty do 3.1.4

---
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -0,0 +1,36 @@
+package edu.kis.vh.nursery;
+
+public class DefaultCountingOutRhymer {
+
+	private int[] numbers = new int[12];
+
+//	Dzialanie kombinacji klawiszy alt + -> oraz alt + <-
+//	Kursor zostaje przestawiony w miejsce, w którym ostatnio stanął kursor po kliknieciu mysza
+
+	public int total = -1;
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+
+		public boolean callCheck() {
+			return total == -1;
+		}
+
+			public boolean isFull() {
+				return total == 11;
+			}
+
+				protected int peekaboo() {
+					if (callCheck())
+						return -1;
+					return numbers[total];
+				}
+
+					public int countOut() {
+						if (callCheck())
+							return -1;
+						return numbers[total--];
+					}
+
+}
---
+++ src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
@@ -0,0 +1,35 @@
+package edu.kis.vh.nursery.list;
+
+public class IntArrayStack {
+
+
+    private int[] numbers = new int[12];
+
+    public int total = -1;
+
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == -1;
+    }
+
+    public boolean isFull() {
+        return total == 11;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return -1;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return -1;
+        return numbers[total--];
+    }
+
+}
