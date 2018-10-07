1129aea7ed7ce4e234eccc7ca2718a9e3c3ad7d2
mokbor <43931480+mokbor@users.noreply.github.com>
2018-10-07 17:05:04 +0200

    3.1.1

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,16 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;//tutaj 3.1.1
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())//tutaj 3.1.1
 			totalRejected++;
-			else
-				super.countIn(in);
+		else //tutaj 3.1.1
+			super.countIn(in);//tutaj 3.1.1.1
 	}
 }
