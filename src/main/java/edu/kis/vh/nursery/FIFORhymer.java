a58f638a561d3180da6d66986c34b5b43ded3d3c
jlopata <jerry.lopata@gmail.com>
2018-10-07 16:38:22 +0200

    Fixed few formatting errors after checking with auto formatting

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,15 +2,15 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends defaultCountingOutRhymer {
 
-	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-	
-	@Override
-	public int countOut() {
-		while (!callCheck())
+    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+
+    @Override
+    public int countOut() {
+        while (!callCheck())
             temp.countIn(super.countOut());
-		int ret = temp.countOut();
-		while (!temp.callCheck())
-		    countIn(temp.countOut());
-		return ret;
-	}
+        int ret = temp.countOut();
+        while (!temp.callCheck())
+            countIn(temp.countOut());
+        return ret;
+    }
 }
