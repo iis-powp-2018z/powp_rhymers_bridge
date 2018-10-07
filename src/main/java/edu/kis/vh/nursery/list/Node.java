9a6d776c66e52f1c249d7f5385962db456e84d4a
yach9 <dominika.jach9@gmail.com>
2018-10-07 17:13:30 +0200

    podpunkty 1-5 lab1

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -8,5 +8,6 @@ public class Node {
 	public Node(int i) {
 		value = i;
 	}
-	
+
 }
+// kombinacja klawiszy alt + -> lub alt + <- powoduje odpowiednio przeskok do kolejnej zakłądki programu w prawo lub lewo.
