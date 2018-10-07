d74da2fb7cf59deced31ec28b1e850f3fcbac953
tirey93 <tirey93@gmail.com>
2018-10-07 16:53:22 +0200

    test2

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -12,7 +12,7 @@ class RhymersDemo {
 		
 		defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
 				factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
-		
+		//test2
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				rhymers[j].countIn(i);
