748beae466c85121f7fe3fd57ba5e982b1daea98
Piotr W <piotrektekkno@interia.pl>
2018-10-12 11:48:20 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
    	modified:   src/main/java/edu/kis/vh/nursery/FIFORhymer.java
    	modified:   src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
    	modified:   src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
    	modified:   src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
    	modified:   src/test/java/edu/kis/vh/nursery/RhymersDemo.java
    	modified:   src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,34 +1,51 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
+public class DefaultCountingOutRhymer {
+	
+	
+	private int testEncapsulateFilled = 0;
+	
+	private static final int EMPTY_STACK_FAIL = -1;
 
 	private int[] NUMBERS = new int[12];
 
-	public int total = -1;
+	private int total = EMPTY_STACK_FAIL; 
 
-	public void countIn(int in) {
+	public int getTotal() {
+		return total;
+	}
+
+	public void countIn(final int in) {
 		if (!isFull())
 			NUMBERS[++total] = in;
 	}
 
-		public boolean callCheck() {
-			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return NUMBERS[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return NUMBERS[total--];
-					}
+	public boolean callCheck() {
+		return total == EMPTY_STACK_FAIL;
+	}
+
+	public boolean isFull() {
+		return total == 11;
+	}
+
+	protected int peekaBoo() {
+		if (callCheck())
+			return EMPTY_STACK_FAIL;
+		return NUMBERS[total];
+	}
+
+	public int countOut() {
+		if (callCheck())
+			return EMPTY_STACK_FAIL;
+		return NUMBERS[total--];
+	}
+
+	public int getTestEncapsulateFilled() {
+		return testEncapsulateFilled;
+	}
+
+	public void setTestEncapsulateFilled(int testEncapsulateFilled) {
+		this.testEncapsulateFilled = testEncapsulateFilled;
+	}
 
 }
