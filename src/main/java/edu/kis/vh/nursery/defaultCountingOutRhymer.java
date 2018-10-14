ec929b74bda0432357ae6e8c03fec412fa0b7697
187670 <madflow12@gmail.com>
2018-10-14 17:08:29 +0200

    L1 3.1.1

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -11,27 +11,25 @@ public class defaultCountingOutRhymer {
 			NUMBERS[++total] = in;
 	}
 
-		public boolean callCheck() {
+	public boolean callCheck() {
 			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return NUMBERS[total];
-				}
+	}
+
+	public boolean isFull() {
+		return total == 11;
+	}
+
+	protected int peekaboo() {
+		if (callCheck())
+			return -1;
+
+		return NUMBERS[total];
+	}
 			
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return NUMBERS[total--];
-					}
+	public int countOut() {
+		if (callCheck())
+			return -1;
 
-					//testing 1 testing 2
-	//test
-
+	return NUMBERS[total--];
+	}
 }
