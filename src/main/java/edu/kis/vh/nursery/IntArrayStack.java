70a6f5d66876704cc45b2a772be716ab0cf79bca
Marcin <marcin.kolaczek@yahoo.pl>
2018-10-14 16:52:48 +0200

    lab 2 bridge 3.1.1

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,44 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+	private static final int fullStack = 11;
+
+	private static final int EmptyStack = -1;
+
+	private static final int stackCapacity = 12;
+
+	private int[] numbers = new int[stackCapacity];
+
+	private int total = EmptyStack;
+
+	protected int getTotal() {
+		return total;
+	}
+
+	protected void countIn(final int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+
+	protected boolean callCheck() {
+		return total == EmptyStack;
+	}
+
+	protected boolean isFull() {
+		return total == fullStack;
+	}
+
+	protected int peekaboo() {
+		if (callCheck())
+			return EmptyStack;
+		return numbers[total];
+	}
+
+	protected int countOut() {
+		if (callCheck())
+			return EmptyStack;
+		return numbers[total--];
+	}
+
+}
