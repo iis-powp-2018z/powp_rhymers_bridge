570180435fadc6b24e3484432dda721dfa982ec2
Marcin <marcin.kolaczek@yahoo.pl>
2018-10-14 16:22:05 +0200

    3.1.12

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -46,3 +46,20 @@ public class IntLinkedList {
 	}
 
 }
+
+class Node {
+
+	protected int value;
+	protected Node prev, next;
+
+	protected Node(final int i) {
+		value = i;
+	}
+
+}
+
+//alt + strzałki
+//Przeskoczenie pomiędzy ostatnio edytowanymi plikami do lokalizacji gdzie wykonywana była ostatia zmiana.
+//Strzałka w lewo cofa się w historii, strzałka w prawo idzie do przodu.
+
+
