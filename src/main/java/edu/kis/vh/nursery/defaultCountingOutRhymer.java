a22d75f4e63423208d2918aab8bbf97f0b0f2106
Damian Biedrowski <damian.biedrowski@gmail.com>
2018-10-28 15:13:47 +0100

    [3.1.2] Use IDE refactoring
    Nothing new happened, except for changing tab indentation
    for space indentation

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,23 +2,23 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-	private int[] NUMBERS = new int[12];
+    private int[] NUMBERS = new int[12];
 
-	public int total = -1;
+    public int total = -1;
 
-	public void countIn(int in) {
-		if (!isFull())
-			NUMBERS[++total] = in;
-	}
+    public void countIn(int in) {
+        if (!isFull())
+            NUMBERS[++total] = in;
+    }
 
     public boolean callCheck() {
-			return total == -1;
-		}
+        return total == -1;
+    }
 
     public boolean isFull() {
-				return total == 11;
-			}
-		
+        return total == 11;
+    }
+
     protected int peekaboo() {
         if (callCheck())
             return -1;
