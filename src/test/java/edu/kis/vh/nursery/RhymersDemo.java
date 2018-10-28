268f3910a9d797c6bb4ea7b6891eb7cd3f7752fa
Damian Biedrowski <damian.biedrowski@gmail.com>
2018-10-28 14:58:19 +0100

    [3.1.1] Refactor the source code, indentation-wise
    HanoiRhymer needed indentation refactoring in the following lines: 5,
    12, 14, 15

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -11,13 +11,13 @@ class RhymersDemo {
 		Rhymersfactory factory = new DefaultRhymersFactory();
 		
 		defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-				factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
+												factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
 		
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
 				rhymers[j].countIn(i);
-		
-		java.util.Random rn = new java.util.Random();
+			java.util.Random rn = new java.util.Random();
+
 		for (int i = 1; i < 15; i++)
 			rhymers[3].countIn(rn.nextInt(20));
 		
@@ -27,9 +27,6 @@ class RhymersDemo {
 			System.out.println();
 		}
 		
-		System.out.println("total rejected is "
-				+ ((HanoiRhymer) rhymers[3]).reportRejected());
-		
+		System.out.println("total rejected is " + ((HanoiRhymer) rhymers[3]).reportRejected());
 	}
-	
 }
