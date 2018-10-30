b850e172a3713914ff9d0dbc83bb92c70f1736f7
90rl <40734302+90rl@users.noreply.github.com>
2018-10-30 21:00:45 +0100

    3.1.11 Annotacja @Override

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -3,7 +3,7 @@ package edu.kis.vh.nursery.factory;
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 
 public interface Rhymersfactory {
-
+	
 	public DefaultCountingOutRhymer getStandardRhymer();
 
 	public DefaultCountingOutRhymer getFalseRhymer();
