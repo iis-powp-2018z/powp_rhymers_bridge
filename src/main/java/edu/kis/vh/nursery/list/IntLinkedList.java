d12c28bb2261073fdffc6bf4d072fb8279612cd1
Monika <monika0991@gmail.com>
2018-11-03 19:53:04 +0100

    3.1.12 Zmiana modyfikatora klasy 'Node' na 'private', usunięcie pliku

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -39,4 +39,36 @@ public class IntLinkedList {
 		return ret;
 	}
 
+	private class Node {
+
+		private final int value;
+		private Node prev;
+		private Node next;
+
+		public Node(int i) {
+			value = i;
+		}
+
+		int getValue() {
+			return value;
+		}
+
+		Node getPrev() {
+			return prev;
+		}
+
+		void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		Node getNext() {
+			return next;
+		}
+
+		void setNext(Node next) {
+			this.next = next;
+		}
+
+	}
+
 }
