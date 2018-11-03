e98b8ad1c7e51f9055246e351764e8a1935a6ea8
Monika <monika0991@gmail.com>
2018-11-03 18:38:12 +0100

    3.1.4 poprawki

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,33 +4,35 @@ public class DefaultCountingOutRhymer {
 
 	private int[] numbers = new int[12];
 
-//	Dzialanie kombinacji klawiszy alt + -> oraz alt + <-
-//	Kursor zostaje przestawiony w miejsce, w którym ostatnio nastapila edycja.
+	// Dzialanie kombinacji klawiszy alt + -> oraz alt + <-
+	// Kursor zostaje przestawiony w miejsce, w którym ostatnio nastapila
+	// edycja.
 
 	public int total = -1;
+
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
-		public boolean callCheck() {
-			return total == -1;
-		}
+	public boolean callCheck() {
+		return total == -1;
+	}
 
-			public boolean isFull() {
-				return total == 11;
-			}
+	public boolean isFull() {
+		return total == 11;
+	}
 
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return numbers[total];
-				}
+	protected int peekaboo() {
+		if (callCheck())
+			return -1;
+		return numbers[total];
+	}
 
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return numbers[total--];
-					}
+	public int countOut() {
+		if (callCheck())
+			return -1;
+		return numbers[total--];
+	}
 
 }
