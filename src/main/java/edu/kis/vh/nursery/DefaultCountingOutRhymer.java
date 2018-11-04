bf7dd0874b58b2acbebd2287a66004159b0ce572
90rl <40734302+90rl@users.noreply.github.com>
2018-11-04 17:26:15 +0100

    3.1.2 tworzenie od nowa klasy FefaultCountingOutRymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,41 +1,12 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
- 
-	private static final int FULL_STACK = 11;
 
-	private static final int TOTAL_START = -1;
+	IntArrayStack arrayStack;
 
-	private static final int NUMBERS_MAX = 12;
-	
-
-	private int[] numbers = new int[NUMBERS_MAX];
-
-	private int total = TOTAL_START;
-
-	public void countIn(int in) {
-		if (!isFull())
-			numbers[++total] = in;
-	}
-
-	protected boolean callCheck() {
-		return total == TOTAL_START;
-	}
-
-	public boolean isFull() {
-		return total == FULL_STACK;
-	}
-
-	protected int peekaboo() {
-		if (callCheck())
-			return TOTAL_START;
-		return numbers[total];
-	}
-
-	public int countOut() {
-		if (callCheck())
-			return -1;
-		return numbers[total--];
+	public DefaultCountingOutRhymer(IntArrayStack arrayStack) {
+		super();
+		this.arrayStack = arrayStack;
 	}
 
 }
