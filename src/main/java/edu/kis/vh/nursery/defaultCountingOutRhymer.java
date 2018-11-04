d5c47c90903bfab9d0f7061ea9a306c8481c52de
Lukas <lukas77pro@o2.pl>
2018-11-04 22:09:18 +0100

    Przywrocono stan po pierwszym pull requescie(lab 1) po przypadkowym usunieciu repozytorium

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -1,34 +1,40 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
+public class DefaultCountingOutRhymer {
+    private static final int STACK_CAPACITY = 12;
+    private static final int EMPTY_STACK_ERROR = -1;
+    private static final int STACK_FULL = STACK_CAPACITY - 1;
 
-	private int[] NUMBERS = new int[12];
+    private final int[] numbers = new int[STACK_CAPACITY];
 
-	public int total = -1;
+    private int total = -1;
 
-	public void countIn(int in) {
-		if (!isFull())
-			NUMBERS[++total] = in;
-	}
+    public int getTotal() {
+        return total;
+    }
 
-		public boolean callCheck() {
-			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return NUMBERS[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return NUMBERS[total--];
-					}
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
 
+    public boolean callCheck() {
+        return total == EMPTY_STACK_ERROR;
+    }
+
+    public boolean isFull() {
+        return total == STACK_FULL;
+    }
+
+    int peekABoo() {
+        if (callCheck())
+            return EMPTY_STACK_ERROR;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return EMPTY_STACK_ERROR;
+        return numbers[total--];
+    }
 }
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,40 +1,74 @@
 package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
+    private static final int EMPTY_VALUE = -1;
 
-	Node last;
-	int i;
+    private Node last;
+    private int i;
 
-	public void push(int i) {
-		if (last == null)
-			last = new Node(i);
-		else {
-			last.next = new Node(i);
-			last.next.prev = last;
-			last = last.next;
-		}
-	}
+    public void push(int i) {
+        if (last == null)
+            last = new Node(i);
+        else {
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
+        }
+    }
 
-	public boolean isEmpty() {
-		return last == null;
-	}
+    private boolean isEmpty() {
+        return last == null;
+    }
 
-	public boolean isFull() {
-		return false;
-	}
+    private boolean isFull() {
+        return false;
+    }
 
-	public int top() {
-		if (isEmpty())
-			return -1;
-		return last.value;
-	}
+    private int top() {
+        if (isEmpty())
+            return EMPTY_VALUE;
+        return last.getValue();
+    }
 
-	public int pop() {
-		if (isEmpty())
-			return -1;
-		int ret = last.value;
-		last = last.prev;
-		return ret;
-	}
+    private int pop() {
+        if (isEmpty())
+            return EMPTY_VALUE;
+        final int ret = last.getValue();
+        last = last.getPrev();
+        return ret;
+    }
 
+    private static class Node {
+        private int value;
+        private Node prev;
+        private Node next;
+
+        Node(int i) {
+            this.setValue(i);
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        void setValue(int value) {
+            this.value = value;
+        }
+
+        Node getPrev() {
+            return prev;
+        }
+
+        void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        Node getNext() {
+            return next;
+        }
+
+        void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
