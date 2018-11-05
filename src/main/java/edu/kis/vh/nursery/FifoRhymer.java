422e61dea3f1279f920bb9d165065a0fb97ac7ea
Kamila <36917682+kamilasyp@users.noreply.github.com>
2018-11-05 14:16:02 +0100

    lab 2 3.1.10

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -1,10 +1,22 @@
 package edu.kis.vh.nursery;
 
+import stack.IntLinkedList;
+
 /**
  * @author Kamila
  *
  */
 public class FifoRhymer extends DefaultCountingOutRhymer {
+
+	public FifoRhymer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FifoRhymer(IntLinkedList intLinkedList) {
+		super(intLinkedList);
+		// TODO Auto-generated constructor stub
+	}
 
 	public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
