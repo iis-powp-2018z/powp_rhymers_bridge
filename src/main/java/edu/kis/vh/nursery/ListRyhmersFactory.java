7c456811faa6b54748fa2f1d68cc8a6015afb126
Kamila <36917682+kamilasyp@users.noreply.github.com>
2018-11-05 14:37:59 +0100

    lab 2 3.1.12

---
+++ src/main/java/edu/kis/vh/nursery/ListRyhmersFactory.java
@@ -0,0 +1,31 @@
+package edu.kis.vh.nursery;
+
+import edu.kis.vh.nursery.factory.Rhymersfactory;
+
+public class ListRyhmersFactory implements Rhymersfactory {
+
+	@Override
+	public DefaultCountingOutRhymer GetStandardRhymer() {
+		// TODO Auto-generated method stub
+		return new DefaultCountingOutRhymer();
+	}
+
+	@Override
+	public DefaultCountingOutRhymer GetFalseRhymer() {
+		// TODO Auto-generated method stub
+		return new DefaultCountingOutRhymer();
+	}
+
+	@Override
+	public DefaultCountingOutRhymer GetFIFORhymer() {
+		// TODO Auto-generated method stub
+		return new HanoiRhymer();
+	}
+
+	@Override
+	public DefaultCountingOutRhymer GetHanoiRhymer() {
+		// TODO Auto-generated method stub
+		return new FifoRhymer();
+	}
+
+}
