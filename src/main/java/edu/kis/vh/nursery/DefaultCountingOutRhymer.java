34e0ce45b1918c4d6557d0408014681ca698fa4b
piotr <piotrektekkno@interia.pl>
2018-11-06 22:26:20 +0100

    Zmiany użycie IntLinkList

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,10 +1,13 @@
 package edu.kis.vh.nursery;
+import edu.kis.vh.nursery.list.IntLinkedList;
 
 public class DefaultCountingOutRhymer {
 	
+	private IntLinkedList intLinkList =  new IntLinkedList();
+	public int total = -1;
 	private IntArrayStack  intArrStk = new IntArrayStack();
 	
-	public DefaultCountingOutRhymer(IntArrayStack intArrStk) {
+	public DefaultCountingOutRhymer(IntLinkedList intLinkList) {
 		super();
 		this.intArrStk = intArrStk;
 	}
@@ -12,17 +15,53 @@ public class DefaultCountingOutRhymer {
 	public DefaultCountingOutRhymer() {
 		
 	}
+
+	public void countIn(int in) {
+		if (!intLinkList.isFull())
+			intLinkList.push(in);
+	}
 	
+	public boolean callCheck() {
+		return intLinkList.isEmpty();
+	}
 	
+	public boolean isFull() {
+		return intLinkList.isFull();
+	}
 	
+	protected int peekaBoo() {
+
+		if (callCheck())
+			return -1;
+		return intLinkList.top();
+
+	}
+	
+	public int countOut() {
+		if (callCheck())
+			return -1;
+		return intLinkList.pop();
+
+	}
+	/*
+	protected int peekaboo() {
+
+		if (callCheck())
+			return -1;
+
+		return intLinkList.
+
+	}
+	
+	*
 	public int getTotal() {
 		return intArrStk.getTotal();
 	}
 
-	public void countIn(int in) {
-		intArrStk.countIn(in);
-	}
-
+	//public void countIn(int in) {
+	//	intArrStk.countIn(in);
+	//}
+/*
 	public boolean callCheck() {
 		return intArrStk.callCheck();
 	}
@@ -34,11 +73,11 @@ public class DefaultCountingOutRhymer {
 	public int countOut() {
 		return intArrStk.countOut();
 	}
-
+/*
 	public int peekaBoo() {
 		return intArrStk.peekaBoo();
 	}
-
+*/
 	public int getTestEncapsulateFilled() {
 		return intArrStk.getTestEncapsulateFilled();
 	}
