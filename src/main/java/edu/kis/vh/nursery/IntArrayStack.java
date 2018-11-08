ef237a511ea8677b51ae7ce2d4b28a244198a837
yach9 <dominika.jach9@gmail.com>
2018-11-08 21:50:27 +0100

    3.1.1Copy implementation from DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/IntArrayStack.java
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -1,61 +1,33 @@
 package edu.kis.vh.nursery;
-
-public class IntArrayStack {
-
-	private static final int FULL_STACK = 11;
-
-	private static final int STACK_CAPACITY = 12;
-
-	private static final int EMPTY_STACK_ERROR = -1;
-
-	private int[] NUMBERS = new int[STACK_CAPACITY];
-
-	public int total = EMPTY_STACK_ERROR;
-
-	public void countIn(int in) {
-		if (!isFull())
-			NUMBERS[++total] = in;
-	}
-
-	public boolean callCheck() {
-		return total == EMPTY_STACK_ERROR;
-	}
-
-	public int getTotal() {
-		return total;
-	}
-
-	public void setTotal(int total) {
-		this.total = total;
-	}
-
-	public boolean isFull() {
-		return total == FULL_STACK;
-	}
-
-	protected int peekaboo() {
-		if (callCheck())
-			return EMPTY_STACK_ERROR;
-		return NUMBERS[total];
-	}
-
-	public int countOut() {
-		if (callCheck())
-			return EMPTY_STACK_ERROR;
-		return NUMBERS[total--];
-	}
-
-	private static int getEmptyStackError() {
-		return EMPTY_STACK_ERROR;
-	}
-
-	private static int getStackCapacity() {
-		return STACK_CAPACITY;
-	}
-
-	private static int getFullStack() {
-		return FULL_STACK;
-	}
-
-} 
- 
+ public class IntArrayStack {
+    private static final int NUMBERS_CAPACITY = 12;
+    private static final int ARRAY_EMPTY = -1;
+    private int[] numbers = new int[NUMBERS_CAPACITY];
+     private int total = ARRAY_EMPTY;
+     public void countIn(int in) {
+        if (!isFull()) {
+            numbers[++total] = in;
+        }
+    }
+     protected boolean callCheck() {
+        return total == ARRAY_EMPTY;
+    }
+     protected boolean isFull() {
+        return total == NUMBERS_CAPACITY -1;
+    }
+     protected int peekaboo() {
+        if (callCheck()) {
+            return ARRAY_EMPTY;
+        }
+        return numbers[total];
+    }
+     public int countOut() {
+        if (callCheck()) {
+            return ARRAY_EMPTY;
+        }
+        return numbers[total--];
+    }
+     public int getTotal() {
+        return total;
+    }
+}
