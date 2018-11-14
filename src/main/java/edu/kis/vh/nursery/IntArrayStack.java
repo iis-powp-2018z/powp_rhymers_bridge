0ee8f56d6740e1ab65f97b13abb5fd451c169222
90rl <40734302+90rl@users.noreply.github.com>
2018-11-14 17:25:56 +0100

    Do dokończenia

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,38 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+	private static final int EMPTY_STACK_SIZE = -1;
+
+	private static final int DEFAULT_STACK_CAPACITY = 12;
+	
+
+	private int[] numbers = new int[DEFAULT_STACK_CAPACITY];
+
+	private int total = EMPTY_STACK_SIZE;
+
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+
+	protected boolean callCheck() {
+		return total == EMPTY_STACK_SIZE;
+	}
+
+	public boolean isFull() {
+		return total == 11;
+	}
+
+	protected int peekaboo() {
+		if (callCheck())
+			return EMPTY_STACK_SIZE;
+		return numbers[total];
+	}
+
+	public int countOut() {
+		if (callCheck())
+			return -1;
+		return numbers[total--];
+	}
+}
