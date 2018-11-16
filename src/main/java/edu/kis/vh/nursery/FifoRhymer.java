a9061ef019d12bf44f5c935d5dde0f8f53b02b1e
mojzesowic <jakubwisn@gmail.com>
2018-11-17 00:08:09 +0100

    3.1.10

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -1,8 +1,15 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.collections.IntLinkedList;
+
 public class FifoRhymer extends DefaultCountingOutRhymer {
 
     private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
+    public FifoRhymer(IntLinkedList intStackImplementation, DefaultCountingOutRhymer temp) {
+        super(intStackImplementation);
+        this.temp = temp;
+    }
 
     @Override
     public int countOut() {
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,11 +1,18 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.collections.IntLinkedList;
+
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     int totalRejected = 0;
 
     protected int reportRejected() {
         return totalRejected;
+    }
+
+    public HanoiRhymer(IntLinkedList intStackImplementation, int totalRejected) {
+        super(intStackImplementation);
+        this.totalRejected = totalRejected;
     }
 
     @Override
