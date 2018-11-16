c26c3256f7f097c257988091706301dfefd05375
kamilasyp <kamilasyp@gmail.com>
2018-11-16 22:58:18 +0100

    lab 2 pkt 8

--- src/main/java/stack/IntArrayStack.java
+++ src/main/java/stack/IntArrayStack.java
@@ -4,9 +4,8 @@ import edu.kis.vh.nursery.Stack;
 
 public class IntArrayStack implements Stack{
     private static final int NUMBERS_CAPACITY = 12;
-    private static final int ARRAY_EMPTY = -1;
     private int[] numbers = new int[NUMBERS_CAPACITY];
-     private int total = ARRAY_EMPTY;
+    private int total = IS_EMPTY;
      
      public void push(int in) {
      	if (!isFull()) {
@@ -14,20 +13,20 @@ public class IntArrayStack implements Stack{
         }
     }
      public boolean isEmpty() {
-        return total == ARRAY_EMPTY;
+        return total == IS_EMPTY;
     }
      public boolean isFull() {
         return total == NUMBERS_CAPACITY -1;
     }
      public int top() {
          if (isEmpty()) {
-            return ARRAY_EMPTY;
+            return IS_EMPTY;
         }
         return numbers[total];
     }
      public int pop() {
          if (isEmpty()) {
-            return ARRAY_EMPTY;
+            return IS_EMPTY;
         }
         return numbers[total--];
     }
