3f248577b7503d26b9fe60766b81e3a4c0c3d2b4
ancalentari <andre45@vp.pl>
2018-11-16 21:07:56 +0100

    3.3. 2 Dokumentacja

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,20 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Implementacja listy dla elementów typu int
+ */
 public class IntLinkedList {
 
+    /**
+     * Ostatni element listy
+     */
     private Node last;
     private int i;
 
+    /**
+     * Metoda dodaje kolejny element typu int na koniec listy
+     * @param i liczba całkowita która ma być dodana do listy
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -15,20 +25,36 @@ public class IntLinkedList {
         }
     }
 
-    public boolean isEmpty() {
+    /**
+     * metda sprawdza czyy lista jest pusta
+     * @return true jeśli lista jest pusta, false jeśli nie jest pusta
+     */
+    private boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * sprawdza czy lista jest pełna
+     * @return false
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * metoda zwraca wartość ostatniego elementu listy
+     * @return wartość ostatniego elementu listy lub -1 jeśli lista jest pusta
+     */
     public int top() {
         if (isEmpty())
             return -1;
         return last.getValue();
     }
 
+    /**
+     * usuwa ostatni element list i zastępuje go przedostatnim
+     * @return ostatni element listy lub -1 jeśli lista jest pusta
+     */
     public int pop() {
         if (isEmpty())
             return -1;
