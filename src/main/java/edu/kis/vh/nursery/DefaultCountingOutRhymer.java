93277d0cf0969314bfc0f1489299ad62b159b8b8
ancalentari <andre45@vp.pl>
2018-11-16 18:58:55 +0100

    6. Modyfikatory public, private 12. Node nie musi być publiczna

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,22 +6,22 @@ public class DefaultCountingOutRhymer {
     private static final int INIT_EMPTY_STACK = -1;
     private int[] NUMBERS = new int[MAX_STACK];
 
-    public int total = INIT_EMPTY_STACK;
+    private int total = INIT_EMPTY_STACK;
 
     public void countIn(int in) {
         if (!isFull())
             NUMBERS[++total] = in;
     }
 
-    public boolean callCheck() {
+    boolean callCheck() {
         return total == INIT_EMPTY_STACK;
     }
 
-    public boolean isFull() {
+    boolean isFull() {
         return total == (MAX_STACK -1);
     }
 
-    protected int peekaboo() {
+    int peekaboo() {
         if (callCheck())
             return -1;
         return NUMBERS[total];
