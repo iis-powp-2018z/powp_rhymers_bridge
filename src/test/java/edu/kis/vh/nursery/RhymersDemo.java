8484f764163bd882949af279bf921af43d2d6c49
ancalentari <andre45@vp.pl>
2018-11-16 20:46:53 +0100

    13. extract static testRhymers

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -3,6 +3,9 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
+import java.util.Arrays;
+import java.util.Random;
+
 class RhymersDemo {
 
 	public static void main(String[] args) {
@@ -10,24 +13,28 @@ class RhymersDemo {
 		
 		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer()};
-		
-		for (int i = 1; i < 15; i++)
-			for (int j = 0; j < 3; j++)
-				rhymers[j].countIn(i);
-		
-		java.util.Random rn = new java.util.Random();
-		for (int i = 1; i < 15; i++)
-			rhymers[3].countIn(rn.nextInt(20));
-		
-		for (int i = 0; i < rhymers.length; i++) {
-			while (!rhymers[i].callCheck())
-				System.out.print(rhymers[i].countOut() + "  ");
-			System.out.println();
-		}
-		
+
+		testRhymers(rhymers);
+
 		System.out.println("total rejected is "
 				+ ((HanoiRhymer) rhymers[3]).reportRejected());
 		
 	}
-	
+
+	private static void testRhymers(DefaultCountingOutRhymer[] rhymers) {
+		for (int i = 1; i < 15; i++)
+			for (int j = 0; j < 3; j++)
+				rhymers[j].countIn(i);
+
+		Random rn = new Random();
+		for (int i = 1; i < 15; i++)
+			rhymers[3].countIn(rn.nextInt(20));
+
+        Arrays.stream(rhymers).forEach(rhymer -> {
+            while (!rhymer.callCheck())
+                System.out.print(rhymer.countOut() + "  ");
+            System.out.println();
+        });
+	}
+
 }
