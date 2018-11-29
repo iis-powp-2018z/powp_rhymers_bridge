f122b22e69204a7e36357164f33e5245bd493e95
90rl <40734302+90rl@users.noreply.github.com>
2018-11-30 00:14:45 +0100

    3.1.6 New localization IntArrayStack class

--- src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
+++ src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
@@ -1,17 +1,46 @@
 package edu.kis.vh.nursery.list;
 
-public interface IntArrayStack {
+public class IntArrayStack {
 
-	void push(int i);
 
-	boolean isEmpty();
+	
+		private static final int FULL_STACK = 11;
 
-	boolean isFull();
+		private static final int TOTAL_START = -1;
 
-	int top();
+		private static final int NUMBERS_MAX = 12;
+		
 
-	int pop();
+		private int[] numbers = new int[NUMBERS_MAX];
 
-	int getSize();
+		private int total = TOTAL_START;
 
+		public void countIn(int in) {
+			if (!isFull())
+				numbers[++total] = in;
+		}
+
+		protected boolean callCheck() {
+			return total == TOTAL_START;
+		}
+
+		public boolean isFull() {
+			return total == FULL_STACK;
+		}
+
+		protected int peekaboo() {
+			if (callCheck())
+				return TOTAL_START;
+			return numbers[total];
+		}
+
+		public int countOut() {
+			if (callCheck())
+				return -1;
+			return numbers[total--];
+		}
+		
+		   public int getTotal() {
+		        return total;
+		    }
 }
