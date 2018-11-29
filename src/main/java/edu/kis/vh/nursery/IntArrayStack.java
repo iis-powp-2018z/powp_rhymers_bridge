3753046231d517614edb3beb14a1ab779f1131cf
90rl <40734302+90rl@users.noreply.github.com>
2018-11-29 23:16:59 +0100

    Lab2 3.1.1 New class IntArrayStack

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,42 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+
+		private static final int FULL_STACK = 11;
+
+		private static final int TOTAL_START = -1;
+
+		private static final int NUMBERS_MAX = 12;
+		
+
+		private int[] numbers = new int[NUMBERS_MAX];
+
+		private int total = TOTAL_START;
+
+		public void countIn(int in) {
+			if (!isFull())
+				numbers[++total] = in;
+		}
+
+		protected boolean callCheck() {
+			return total == TOTAL_START;
+		}
+
+		public boolean isFull() {
+			return total == FULL_STACK;
+		}
+
+		protected int peekaboo() {
+			if (callCheck())
+				return TOTAL_START;
+			return numbers[total];
+		}
+
+		public int countOut() {
+			if (callCheck())
+				return -1;
+			return numbers[total--];
+		}
+
+}
