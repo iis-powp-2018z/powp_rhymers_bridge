7bcaf21b600a226bade4a8260673696834fdff07
Wojtek Niedzielski <nwojtek@gmail.com>
2019-01-09 23:10:19 +0100

    lab. 2, 3.1 zad. 1

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,39 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+    private static final int full = 11;
+    private static final int wrongVal = -1;
+    private final int[] NUMBERS = new int[12];
+
+    private int total = wrongVal;
+
+    public void countIn(int in) {
+        if (!isFull())
+            NUMBERS[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == wrongVal;
+    }
+
+    public boolean isFull() {
+        return total == full;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return wrongVal;
+        return NUMBERS[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return wrongVal;
+        return NUMBERS[total--];
+    }
+
+    public int getTotal() {
+        return total;
+    }
+}
