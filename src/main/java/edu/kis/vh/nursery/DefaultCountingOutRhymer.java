baf586e8e7ce6b5227c3e236a82857cf2adeb8bc
Wojtek Niedzielski <nwojtek@gmail.com>
2019-01-09 16:32:38 +0100

    lab. 1, zad. 7

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -33,4 +33,12 @@ public class DefaultCountingOutRhymer {
         return NUMBERS[total--];
     }
 
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
 }
